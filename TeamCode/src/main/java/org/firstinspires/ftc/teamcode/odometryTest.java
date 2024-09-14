973372501b4f531331df8d1a396252c40a47bb5d
Thomas <tlrapalski@mcdonogh.org>
2024-09-14 16:41:10 -0400

    test commit and push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryTest.java
@@ -1,6 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-import
 
-public class odometryTest {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+// This is far side Blue
+@Autonomous(name="testsForOdometry", group="Auto2022")
+public class odometryTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontRight = null;
+    private DcMotor frontLeft = null;
+    private DcMotor backRight = null;
+    private DcMotor backLeft = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
