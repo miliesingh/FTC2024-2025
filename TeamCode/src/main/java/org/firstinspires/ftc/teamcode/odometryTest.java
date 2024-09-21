4e05582c717064f98e3fc2e3add19778f8656a2d
Thomas <tlrapalski@mcdonogh.org>
2024-09-21 17:45:39 -0400

    worked on odometry stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometryTest.java
@@ -1,6 +1,6 @@
-
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -11,19 +11,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
-
-
-
-
 @TeleOp(name="odometryTest", group="Linear Opmode")
 public class odometryTest extends LinearOpMode {
     private DcMotor backLeft = null;
-    private DcMotor  backRight = null;
+    private DcMotor backRight = null;
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
-
-    private OdometryPod
 
 
     static final double MAX_POS = 0.5;
@@ -39,10 +32,10 @@ public class odometryTest extends LinearOpMode {
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        backLeft  = hardwareMap.get(DcMotor.class, "back_left");
-        backRight = hardwareMap.get(DcMotor.class, " back_right");
-        frontLeft  = hardwareMap.get(DcMotor.class, "front_left");
-        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, " backRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
@@ -52,6 +45,9 @@ public class odometryTest extends LinearOpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        final int orginalPosFrontRight = frontRight.getCurrentPosition();
+        final int orginalPosFrontLeft = frontLeft.getCurrentPosition();
+        final int turn = backRight.getCurrentPosition();
 
 
         // Wait for the game to start (driver presses PLAY)
@@ -59,22 +55,22 @@ public class odometryTest extends LinearOpMode {
         runtime.reset();
 
         //  run until the end of the match (driver presses STOP)
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
+
             double max;
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = -1*(axial + lateral + yaw);
-            double rightFrontPower = -1*(axial - lateral - yaw);
-            double leftBackPower   = -1*(axial - lateral + yaw);
-            double rightBackPower  = -1*(axial + lateral - yaw);
+            double leftFrontPower = -1 * (axial + lateral + yaw);
+            double rightFrontPower = -1 * (axial - lateral - yaw);
+            double leftBackPower = -1 * (axial - lateral + yaw);
+            double rightBackPower = -1 * (axial + lateral - yaw);
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
@@ -83,10 +79,10 @@ public class odometryTest extends LinearOpMode {
             max = Math.max(max, Math.abs(rightBackPower));
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
+                leftFrontPower /= max;
                 rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
             }
 
 
@@ -99,33 +95,28 @@ public class odometryTest extends LinearOpMode {
 
 
             // Show the elapsed game time and wheel power.
+
+
+
+
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("turnL", frontRight.getCurrentPosition() - orginalPosFrontRight);
+            telemetry.addData("turnR", frontLeft.getCurrentPosition() - orginalPosFrontLeft);
+            telemetry.addData("turnB", backRight.getCurrentPosition() - turn);
 
+
+
+
+
+            telemetry.update();
+            //reading odometry pods
             //Use the odometry to do stuff
-            delta_left_encoder_pos = left_encoder_pos - prev_left_encoder_pos;
-            delta_right_encoder_pos = right_encoder_pos - prev_right_encoder_pos
-            delta_center_encoder_pos = center_encoder_pos - prev_center_encoder_pos
 
-            phi = (delta_left_encoder_pos - delta_right_encoder_pos) / trackwidth
-            delta_middle_pos = (delta_left_encoder_pos + delta_right_encoder_pos) / 2
-            delta_perp_pos = delta_center_encoder_pos - forward_offset * phi
-
-            delta_x = delta_middle_pos * cos(heading) - delta_perp_pos * sin(heading)
-            delta_y = delta_middle_pos * sin(heading) + delta_perp_pos * cos(heading)
-
-            x_pos += delta_x
-            y_pos += delta_y
-            heading += phi
-
-            prev_left_encoder_pos = left_encoder_pos
-            prev_right_encoder_pos = right_encoder_pos
-            prev_center_encoder_pos = center_encoder_pos
-
+            //
 
 
         }
-
     }
 }
